1f518509f4594f6b412f78e0bd544334cebedfc0
RJ <rodney@rj-s.it>
2014-08-29 18:50:25 -0700

    first commit in separate-project

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<title>HI!</title>
+</head>
+<body>
+<h1>HI!</h1>
+<hr>
+<pre><?php
+
+echo (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? "YAY! SSL!" : "NON-SSL";
+echo "\n\n";
+
+?></pre>
+
+<?php if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') { ?>
+	<hr />
+	<pre><?php print_r($_SERVER); ?></pre>
+<?php } else { ?>
+<p>Changing to SSL in 3 seconds...</p>
+<script type="text/javascript">
+setTimeout(goSSL, 3000);
+
+function goSSL()
+{
+	window.location.href = 'https://dev60/index.php';
+}
+</script>
+<?php } ?>
+</body>
+</html>
